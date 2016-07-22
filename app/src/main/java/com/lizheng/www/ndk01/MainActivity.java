ef93c57939aa90cbf9efc70d25acad1ac74beb3d
lizheng <lixiaozheng.good@163.com>
2016-07-22 19:38:56 +0800

    add button endittext and textview run Hello_Jni

--- app/src/main/java/com/lizheng/www/ndk01/MainActivity.java
+++ app/src/main/java/com/lizheng/www/ndk01/MainActivity.java
@@ -2,12 +2,26 @@ package com.lizheng.www.ndk01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    Button button;
+    EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tv);
+        button = (Button) findViewById(R.id.button);
+        input = (EditText) findViewById(R.id.input);
+
+
+        tv.setText(new MyNdk().getString());
     }
 }
---
+++ app/src/main/java/com/lizheng/www/ndk01/MyNdk.java
@@ -0,0 +1,13 @@
+package com.lizheng.www.ndk01;
+
+/**
+ * Created by 10648 on 2016/7/22 0022.
+ */
+public class MyNdk {
+
+    static {
+        System.loadLibrary("MyLibrary");
+    }
+
+    public native String getString();
+}
